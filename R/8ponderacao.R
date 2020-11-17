37f5870d312d365ed9fcc38eb926cc27e6afb7a1
Antônio Augusto Moura da Silva <69313688+aamouradasilva@users.noreply.github.com>
2020-11-17 17:18:23 -0300

    Update 8ponderacao.R

--- R/8ponderacao.R
+++ R/8ponderacao.R
@@ -7,40 +7,40 @@
 install.packages("survey")
 
 # Construindo o banco de dados
-psf<- rbind (c(1,1,0.4), c(1,0,0.20), c(1,1,0.30), c(1,1,0.45), c(1,1,0.40), c(0,0,0.30), c(0,0,0.40), c(0,1,0.50), c(0,0,0.10), c(0,0,0.15)) 
-psf<-as.data.frame (psf) 
-names (psf) <-c("psf", "pobreza", "vac")
+esf<- rbind (c(1,1,0.4), c(1,0,0.20), c(1,1,0.30), c(1,1,0.45), c(1,1,0.40), c(0,0,0.30), c(0,0,0.40), c(0,1,0.50), c(0,0,0.10), c(0,0,0.15)) 
+esf<-as.data.frame (esf) 
+names (esf) <-c("esf", "pobreza", "vac")
 
 # Obtendo listagem do banco de dados
-psf
+esf
 
-# Calculando a média da vacinação segundo participação no PSF
-tapply(psf$vac, psf$psf, summary)
+# Calculando a média da vacinação segundo participação no esf
+tapply(esf$vac, esf$esf, summary)
 
 # Rodando regressão logística com tratamento e confundidor
 # Estimando o escore de propensão
-psflog<-glm(psf ~ pobreza, family="binomial", data=psf)
-summary(psflog)
+esflog<-glm(esf ~ pobreza, family="binomial", data=esf)
+summary(esflog)
 
 # Calculando e incluindo o escore de propensão no banco de dados
-psf$ep <- predict(psflog,type="response")
+esf$ep <- predict(esflog,type="response")
 
 # Listagem do banco de dados com o escore de propensão
-list(psf)
+list(esf)
 
 # Ponderação com escore de propensão
 # calculando os pesos 
 # para o grupo de tratamento = 1/ep
 # para o grupo controle= 1(1-ep)
-psf$peso <- ifelse(psf$psf == 1, 1/psf$ep, 1/(1-psf$ep))
+esf$peso <- ifelse(esf$esf == 1, 1/esf$ep, 1/(1-esf$ep))
 
 # Listagem do banco de dados com o peso
-list(psf)
+list(esf)
 
 # Cálculo do efeito causal em modelo explicativo
 # ponderado pelo inverso da probabilidade de seleção
 # carregando pacote survey
 library(survey)
-pslog <- svydesign(ids=~1,weights=~peso, data=psf)
-reg <- svyglm(vac ~ psf, design=pslog)
+pslog <- svydesign(ids=~1,weights=~peso, data=esf)
+reg <- svyglm(vac ~ esf, design=pslog)
 summary(reg)
