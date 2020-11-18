d34a88c619c5362211bcb10d19adc5a6fa9b60c8
Antônio Augusto Moura da Silva <69313688+aamouradasilva@users.noreply.github.com>
2020-11-18 17:21:27 -0300

    Update 9escore.R

--- R/9escore.R
+++ R/9escore.R
@@ -4,16 +4,16 @@
 # página 182
 
 # Construindo o banco de dados
-psf<- rbind (c(1,0.25,2), c(1,1,4), c(1,0.75,6), c(1,0.50,2), c(1,0.50,8), c(0,1,3), c(0,0.75,7), c(0,0.50,5), c(0,2,10), c(0,3,15)) 
-psf<-as.data.frame (psf) 
-names (psf) <-c("psf", "renda", "escolaridade")
+esf<- rbind (c(1,0.25,2), c(1,1,4), c(1,0.75,6), c(1,0.50,2), c(1,0.50,8), c(0,1,3), c(0,0.75,7), c(0,0.50,5), c(0,2,10), c(0,3,15)) 
+esf<-as.data.frame (esf) 
+names (esf) <-c("esf", "renda", "escolaridade")
   
 # Modelo de regressão logística
-psflog<-glm(psf ~ renda + escolaridade, family=binomial(), data=psf)
-summary(psflog)
+esflog<-glm(esf ~ renda + escolaridade, family=binomial(), data=esf)
+summary(esflog)
 
 # Incluindo o escore de propensão no banco de dados
-psf$ep <- predict(psflog,type="response")
+esf$ep <- predict(esflog,type="response")
 
 # Listagem do banco de dados com o escore de propensão
-list(psf)
+list(esf)
