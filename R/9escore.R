7ab60f0330cddcde5959398334d941fc8da6368e
Antônio Augusto Moura da Silva <69313688+aamouradasilva@users.noreply.github.com>
2020-11-30 11:51:33 -0300

    Update 9escore.R

--- R/9escore.R
+++ R/9escore.R
@@ -3,6 +3,10 @@
 # 9.2.1. Script para estimar o escore de propensão por meio de regressão logística no R
 # página 182
 
+# arquivo salvo em UTF-8
+# Se os caracteres acentuados e a cedilha não aparecerem corretamente
+# No R Studio, escolha a opção File : Reopen with Encoding: UTF-8
+
 # Construindo o banco de dados
 esf<- rbind (c(1,0.25,2), c(1,1,4), c(1,0.75,6), c(1,0.50,2), c(1,0.50,8), c(0,1,3), c(0,0.75,7), c(0,0.50,5), c(0,2,10), c(0,3,15)) 
 esf<-as.data.frame (esf) 
